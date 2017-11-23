974aef0d2748cb84d8928997b9f16d078c36445e
TiagoLeite <tiago.tmleite@gmail.com>
2017-11-22 22:30:21 -0200

    video rec

--- app/src/main/java/br/usp/trabalhoandroid/MainActivity.java
+++ app/src/main/java/br/usp/trabalhoandroid/MainActivity.java
@@ -1,13 +1,19 @@
 package br.usp.trabalhoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, VideoRecordActivity.class);
+        startActivity(intent);
+
     }
 }
