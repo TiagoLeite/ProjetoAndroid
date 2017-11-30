a3746939fa4e46b6035a846541486c4b4bdccfc0
TiagoLeite <tiago.tmleite@gmail.com>
2017-11-30 21:48:01 -0200

    new drawer layout

--- app/src/main/java/br/usp/trabalhoandroid/LoginActivity.java
+++ app/src/main/java/br/usp/trabalhoandroid/LoginActivity.java
@@ -7,9 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Created by Victor on 28/11/2017.
- */
 
 public class LoginActivity extends AppCompatActivity{
 
--- app/src/main/java/br/usp/trabalhoandroid/RegisterFragment.java
+++ app/src/main/java/br/usp/trabalhoandroid/RegisterFragment.java
@@ -2,35 +2,45 @@ package br.usp.trabalhoandroid;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 
-public class RegisterActivity extends AppCompatActivity {
-
+public class RegisterFragment extends Fragment
+{
     EditText etBirthDateDay, etBirthDateMonth, etBirthDateYear, etName, etEmail;
     TextView t;
     RadioButton rbSexM, rbSexF;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_register);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        etName = (EditText) findViewById(R.id.etName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        etBirthDateDay = (EditText) findViewById(R.id.etBirthDateDay);
-        etBirthDateMonth = (EditText) findViewById(R.id.etBirthDateMonth);
-        etBirthDateYear = (EditText) findViewById(R.id.etBirthDateYear);
-        t = (TextView) findViewById(R.id.titler);
-        rbSexF = (RadioButton) findViewById(R.id.rbSexF);
-        rbSexM = (RadioButton) findViewById(R.id.rbSexM);
+        etName = (EditText)view.findViewById(R.id.etName);
+        etEmail = (EditText) view.findViewById(R.id.etEmail);
+        etBirthDateDay = (EditText) view.findViewById(R.id.etBirthDateDay);
+        etBirthDateMonth = (EditText) view.findViewById(R.id.etBirthDateMonth);
+        etBirthDateYear = (EditText) view.findViewById(R.id.etBirthDateYear);
+        t = (TextView) view.findViewById(R.id.titler);
+        rbSexF = (RadioButton) view.findViewById(R.id.rbSexF);
+        rbSexM = (RadioButton) view.findViewById(R.id.rbSexM);
 
+        setupViews();
 
+        return view;
+    }
+
+    private void setupViews()
+    {
         //TextListeners change the focus of the Date of Birth EditTexts as the user types the day, month and year
         etBirthDateDay.addTextChangedListener(new TextWatcher() {
             @Override
@@ -88,10 +98,5 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
     }
 }
